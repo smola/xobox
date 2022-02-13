475c6ee9a1fd35028d0a59506b43482d7490f082
Santiago M. Mola <santi@mola.io>
2022-02-13 13:52:45 +0100

    fix: support non-utf8 arguments (#8)

--- src/cmd/cmd_false.rs
+++ src/cmd/cmd_false.rs
@@ -1,6 +1,7 @@
 use super::base::Command;
+use std::ffi::OsString;
 
 pub const COMMAND: Command = Command {
     name: "false",
-    run: |_args: &[String]| -> i32 { 1 },
+    run: |_args: &[OsString]| -> i32 { 1 },
 };
--- tests/basename_test.rs
+++ tests/basename_test.rs
@@ -1,5 +1,9 @@
 mod common;
 use duct::cmd;
+use std::ffi::OsStr;
+
+#[cfg(not(windows))]
+use std::os::unix::ffi::OsStrExt;
 
 #[test]
 fn test_basename() {
@@ -8,6 +12,22 @@ fn test_basename() {
         .unchecked()
         .run()
         .unwrap();
+    assert_eq!(Some(0), res.status.code());
+    assert_eq!(Vec::from("base\n"), res.stdout);
+}
+
+#[cfg(not(windows))]
+#[test]
+fn test_basename_non_utf8() {
+    let res = cmd!(
+        common::get_bin(),
+        OsStr::from_bytes(b"basename"),
+        OsStr::from_bytes(b"\xFF/base")
+    )
+    .stdout_capture()
+    .unchecked()
+    .run()
+    .unwrap();
     assert_eq!(Some(0), res.status.code());
     assert_eq!(Vec::from("base\n"), res.stdout);
 }
