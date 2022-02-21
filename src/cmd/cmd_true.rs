ed2b3e329e4509fbcaf7989f6087f91fa906da53
Santiago M. Mola <santi@mola.io>
2022-02-21 20:01:26 +0100

    refactor: increase test coverage, fix minor issues (#10)
    Skip a test that segfaults with tarpaulin, and add a test job without
    coverage.

--- src/cmd/cmd_true.rs
+++ src/cmd/cmd_true.rs
@@ -1,7 +1,8 @@
 use super::base::Command;
 use std::ffi::OsString;
 
-pub const COMMAND: Command = Command {
-    name: "true",
-    run: |_args: &[OsString]| -> i32 { 0 },
-};
+fn run(_args: &[OsString]) -> i32 {
+    0
+}
+
+pub const COMMAND: Command = Command { name: "true", run };
---
+++ tests/help_test.rs
@@ -0,0 +1,59 @@
+mod common;
+use duct::cmd;
+use pretty_assertions::assert_eq;
+
+#[test]
+fn test_help() {
+    let res = cmd!(common::get_bin())
+        .stdout_capture()
+        .unchecked()
+        .run()
+        .unwrap();
+    assert_eq!(Some(0), res.status.code());
+    assert_eq!(
+        "xobox\n\nFunctions:\n\tbasename\n\tfalse\n\ttrue\n\tyes\n",
+        String::from_utf8(res.stdout).unwrap()
+    );
+}
+
+#[test]
+fn test_help_flag_help() {
+    let res = cmd!(common::get_bin(), "--help")
+        .stdout_capture()
+        .unchecked()
+        .run()
+        .unwrap();
+    assert_eq!(Some(0), res.status.code());
+    assert_eq!(
+        "xobox\n\nFunctions:\n\tbasename\n\tfalse\n\ttrue\n\tyes\n",
+        String::from_utf8(res.stdout).unwrap()
+    );
+}
+
+#[test]
+fn test_help_flag_h() {
+    let res = cmd!(common::get_bin(), "-h")
+        .stdout_capture()
+        .unchecked()
+        .run()
+        .unwrap();
+    assert_eq!(Some(0), res.status.code());
+    assert_eq!(
+        "xobox\n\nFunctions:\n\tbasename\n\tfalse\n\ttrue\n\tyes\n",
+        String::from_utf8(res.stdout).unwrap()
+    );
+}
+
+#[test]
+fn test_help_invalid_program() {
+    let res = cmd!(common::get_bin(), "bad")
+        .stdout_capture()
+        .unchecked()
+        .run()
+        .unwrap();
+    assert_eq!(Some(1), res.status.code());
+    assert_eq!(
+        "Invalid program: bad\n",
+        String::from_utf8(res.stdout).unwrap()
+    );
+}
