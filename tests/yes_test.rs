59f5fb9a866f41c1e7c41ad638e1c4eeccb3ccf3
Santiago M. Mola <santi@mola.io>
2022-02-23 19:10:24 +0100

    feat: optimize yes command by using a buffer (#12)
    See https://www.reddit.com/r/unix/comments/6gxduc/how_is_gnu_yes_so_fast/
    
    Also ignore SIGPIPE.

--- tests/yes_test.rs
+++ tests/yes_test.rs
@@ -28,3 +28,31 @@ fn test_yes_with_arg() {
         String::from_utf8(res.stdout).unwrap()
     );
 }
+
+#[test]
+fn test_yes_with_large_arg() {
+    let arg = std::iter::repeat("X").take(9000).collect::<String>();
+    let res = cmd!(common::get_bin(), "yes", arg.clone())
+        .pipe(cmd!("head", "-n", "1"))
+        .stdout_capture()
+        .unchecked()
+        .run()
+        .unwrap();
+    assert_eq!(Some(0), res.status.code());
+    assert_eq!(arg + "\n", String::from_utf8(res.stdout).unwrap());
+}
+
+#[test]
+fn test_yes_with_multiple_arg() {
+    let res = cmd!(common::get_bin(), "yes", "yes", "no")
+        .pipe(cmd!("head", "-n", "5"))
+        .stdout_capture()
+        .unchecked()
+        .run()
+        .unwrap();
+    assert_eq!(Some(0), res.status.code());
+    assert_eq!(
+        "yes no\nyes no\nyes no\nyes no\nyes no\n",
+        String::from_utf8(res.stdout).unwrap()
+    );
+}
